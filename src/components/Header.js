cc5f91d4eb0e51711d3b45033a9ec281c6590883
Andy <andy.park@nafinc.com>
2021-04-14 20:17:10 -0700

    init compononets

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <h1 class="header">
+                Stonks
+            </h1>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/TickerDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TickerDetail() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TickerDetail
